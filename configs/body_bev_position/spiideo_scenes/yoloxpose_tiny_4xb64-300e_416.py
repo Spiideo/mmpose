9960d4894f00f6c4700f0ffd1e4949e6cd5839a6
Hakan Ardo <hakan.ardo@spiideo.com>
2024-09-12 14:27:15 +0200

    more workers

--- configs/body_bev_position/spiideo_scenes/yoloxpose_tiny_4xb64-300e_416.py
+++ configs/body_bev_position/spiideo_scenes/yoloxpose_tiny_4xb64-300e_416.py
@@ -3,9 +3,9 @@ _base_ = [
     'soccer_v1.py',
 ]
 
-train_dataloader = dict(dataset=_base_.train_dataset)
-val_dataloader = dict(dataset=_base_.val_dataset)
-test_dataloader = dict(dataset=_base_.test_dataset)
+train_dataloader = dict(dataset=_base_.train_dataset, num_workers=16)
+val_dataloader = dict(dataset=_base_.val_dataset, num_workers=4)
+test_dataloader = dict(dataset=_base_.test_dataset, num_workers=4)
 val_evaluator = dict(ann_file=_base_.val_evaluator_ann_file, iou_type='bbox')
 test_evaluator = dict(ann_file=_base_.test_evaluator_ann_file, iou_type='bbox')
 
