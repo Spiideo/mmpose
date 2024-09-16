3e00ebb5559aa53ceea85d063a08ab153c55dac3
Hakan Ardo <hakan.ardo@spiideo.com>
2024-09-16 12:47:40 +0200

    clearml, 1280

---
+++ configs/body_bev_position/spiideo_scenes/yoloxpose_tiny_4xb64-300e_640.py
@@ -0,0 +1,17 @@
+_base_ = [
+    '../../body_2d_keypoint/yoloxpose/coco/yoloxpose_tiny_4xb64-300e_coco-640.py',
+    'soccer_v1.py',
+]
+
+train_dataloader = dict(dataset=_base_.train_dataset, num_workers=16, batch_size=64)
+val_dataloader = dict(dataset=_base_.val_dataset, num_workers=4)
+test_dataloader = dict(dataset=_base_.test_dataset, num_workers=4)
+val_evaluator = _base_.bev_val_evaluator
+test_evaluator = _base_.bev_test_evaluator
+visualizer = _base_.clearml_visualizer
+
+model = dict(head=dict(
+    num_keypoints=2,
+    assigner=dict(oks_calculator=dict(type='PoseOKS', metainfo=_base_.dataset_metainfo)),
+    loss_oks=dict(metainfo=_base_.dataset_metainfo),
+))
