5081ca78931e82ec41a3360b9d30ef0d748ac24d
Hakan Ardo <hakan@debian.org>
2024-09-17 17:09:23 +0200

    num_last_epochs=100

--- configs/body_bev_position/spiideo_scenes/yoloxpose_tiny_4xb64-300e_640.py
+++ configs/body_bev_position/spiideo_scenes/yoloxpose_tiny_4xb64-300e_640.py
@@ -20,3 +20,4 @@ model = dict(head=dict(
 optim_wrapper = dict(optimizer=dict(lr=0.0001))
 param_scheduler = _base_.param_scheduler
 param_scheduler[1]['eta_min'] = 0.000005
+custom_hooks[0]['num_last_epochs'] = 100
