6680169435dd8f88174b722af9c87cde669d0386
Hakan Ardo <hakan.ardo@spiideo.com>
2024-09-26 13:45:40 +0200

    plot

--- plot_test_results.py
+++ plot_test_results.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import json
+from matplotlib import pyplot as plt
 
 gflops = {
         ('yolox-tiny', 640): 10.3,
@@ -17,7 +18,11 @@ gflops = {
 }
 
 
+class Resolution(int):
+    pass
+
 work_dirs = Path('work_dirs')
+rows = []
 for snap in work_dirs.rglob('**/epoch_300.pth'):
     for fn in sorted(snap.parent.rglob('**/*.json'), reverse=True):
         if fn.name == fn.parent.name + '.json':
@@ -27,11 +32,48 @@ for snap in work_dirs.rglob('**/epoch_300.pth'):
     stats = json.load(fn.open())
     if "test/locsim/AP" not in stats:
         continue
-    print(fn)
 
     parts = snap.parent.name.split('_')
     name = 'yolox-' + parts[1]
     res = int(parts[3])
 
-    row = [name, f'${res} \times {res}$', gflops[name, res], ]
-    str(fn)
+    stats.keys()
+
+    common = [Resolution(res), gflops[name, res], stats['test/bbox/AP']*100]
+    rows.append([name + ' bbox'] + common + [stats['test/locsim_bbox/AP']*100, stats['test/locsim_bbox/precision']*100, stats['test/locsim_bbox/recall']*100, stats['test/locsim_bbox/f1']*100])
+    rows.append([name + ' pose'] + common + [stats['test/locsim/AP']*100, stats['test/locsim/precision']*100, stats['test/locsim/recall']*100, stats['test/locsim/f1']*100])
+
+def fmt(val):
+    if isinstance(val, str):
+        return val
+    elif isinstance(val, Resolution):
+        return f'${val} \\times {val}$'
+
+    return f'{val:.1f}'
+
+def skey(r):
+    return (r[0].split(' ')[1], r[1], r[2])
+
+rows.sort(key=skey)
+prev_res = None
+for r in rows:
+    if prev_res is not None and prev_res != r[1]:
+        print('        \\vspace{-3mm} \\\\\n')
+    print('        ' + ' & '.join(fmt(c) for c in r) + '\\\\')
+    prev_res = r[1]
+
+def plot_set(name, res, style, color):
+    rr = [r for r in rows if name in r[0] and r[1]==res]
+    plt.plot([r[2] for r in rr], [r[4] for r in rr], style, label=f'{name} {res}x{res}', color=color)
+
+colors = ['#d7191c','#fdae61','#2c7bb6','#abd9e9']
+
+plt.clf()
+for i, res in enumerate(sorted(set([r[1] for r in rows]), reverse=True)):
+    plot_set('pose', res, '-o', colors[i])
+for i, res in enumerate(sorted(set([r[1] for r in rows]), reverse=True)):
+    plot_set('bbox', res, '-o', colors[i + 2])
+plt.legend()
+plt.xlabel('GFLOPS')
+plt.ylabel('mAP-LocSim')
+plt.savefig('locsim.pdf')
